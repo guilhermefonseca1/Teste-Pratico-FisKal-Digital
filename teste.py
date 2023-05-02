ce66abf30e27c88f2833340007179d480dfba803
guilhermefonseca1 <guilherme.limasoares@hotmail.com>
2023-05-01 21:06:27 -0300

    Inserindo o arquivo chromedrive

--- teste.py
+++ teste.py
@@ -1,21 +1,25 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import pandas as pd
+#0 - Ler o arquivo Excel
+#1 - Passar pelas linhas do arquivo Excel
+  #1-1- Preencher o formulário do navegador com as informações do Excel
+#(manipular o navegador com o selenium)
+
+#O Selenium é uma biblioteca que auxilia na manipulação e interação com o navegador
+from selenium import webdriver #Auxila na manipula do navegador
+from selenium.webdriver.common.by import By #O By é utilizado para encontrar as tags dos input, por exemplo.
+from selenium.webdriver.common.keys import Keys #O Keys é usado para inserir dados nos inputs
+import pandas as pd #Lê o arquivo excel
 import time
 
-nome_do_arquivo = "challenge.xlsx"
+excel_file_path = "challenge.xlsx"
 url_do_forms = "https://www.rpachallenge.com/"
 
-df = pd.read_excel(nome_do_arquivo)
+data = pd.read_excel(excel_file_path) #Lendo o arquivo excel usando o pandas
 
-#print(df.info())
 
-for index,row in df.iterrows():
-    #print("index: " + str(index) + "E o nome do fulano é " + row["First Name"])
-    chrome = webdriver.Chrome()
-    chrome.get(url_do_forms)
-
+for index,colunm in data.iterrows(): #O código pega o índice e a linha e retorna os elementos dentro dessa linha como um objeto
+    chrome = webdriver.Chrome(executable_path='chromedriver.exe') #Cria uma instância do chrome. O chromedrive manipula o googlechrome
+    chrome.get(url_do_forms) #Depois de Cria uma instância do chrome, navegue para essa url
+#
     time.sleep(0.5)
 
     chrome.find_element(By.XPATH, '//button[@class="waves-effect col s12 m12 l12 btn-large uiColorButton"]').click()
@@ -27,13 +31,13 @@ for index,row in df.iterrows():
     elemento_texto_Email = chrome.find_element(By.XPATH,'//input[@ng-reflect-name="labelEmail"]')
     elemento_texto_Last_Name= chrome.find_element(By.XPATH,'//input[@ng-reflect-name="labelLastName"]')
 
-    elemento_texto_First_Name.send_keys(row["First Name"])
-    elemento_texto_Role_in_company.send_keys(row["Role in Company"])
-    elemento_texto_Address.send_keys(row["Address"])
-    elemento_texto_Phone_Number.send_keys(row["Phone Number"])
-    elemento_texto_Company_Name.send_keys(row["Company Name"])
-    elemento_texto_Email.send_keys(row["Email"])
-    elemento_texto_Last_Name.send_keys(row["Last Name "])
+    elemento_texto_First_Name.send_keys(colunm["First Name"]) #No elemento_text... insira o elemento da linha chamado First Name
+    elemento_texto_Role_in_company.send_keys(colunm["Role in Company"])
+    elemento_texto_Address.send_keys(colunm["Address"])
+    elemento_texto_Phone_Number.send_keys(colunm["Phone Number"])
+    elemento_texto_Company_Name.send_keys(colunm["Company Name"])
+    elemento_texto_Email.send_keys(colunm["Email"])
+    elemento_texto_Last_Name.send_keys(colunm["Last Name "])
 
     chrome.find_element(By.XPATH, '//input[@value="Submit"]').click()
-    #chrome.quit
+    
